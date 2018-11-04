7363623b4e841ff97ceed5f5279a277f9898d684
Geecek <gicalamaciej@gmail.com>
2018-11-04 23:23:34 +0100

    Add pushToStack method

--- App.js
+++ App.js
@@ -1,36 +1,50 @@
 import React, {Component} from 'react'
-import {StyleSheet, View, Button} from 'react-native'
+import {StyleSheet, View, Button, Text} from 'react-native'
 
 export default class App extends Component {
-  render() {
-    return (
+  constructor(props) {
+    super(props)
+    this.state = {
+      stack: [],
+      buttons: ['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '*', '0', '/']
+    }
+  }
+  pushToStack(index) {
+    this.setState(({stack, buttons}) => {
+      return {
+        stack: stack.concat([buttons[index]])
+      }
+    })
+  }
+
+  render() {return (
       <View style={styles.container}>
-        <View style={styles.screen}></View>
+      <View style={styles.screen}><Text>{this.state.stack}</Text></View>
         <View style={styles.row}>
           <View style={styles.button}><Button title="Clear"></Button></View>
           <View style={styles.button}><Button title="Backspace"></Button></View>
         </View>
         <View style={styles.row}>
-          <View style={styles.button}><Button title="7"></Button></View>
-          <View style={styles.button}><Button title="8"></Button></View>
-          <View style={styles.button}><Button title="9"></Button></View>
-          <View style={styles.button}><Button title="+"></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[0]} onPress={this.pushToStack.bind(this, 0)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[1]} onPress={this.pushToStack.bind(this, 1)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[2]} onPress={this.pushToStack.bind(this, 2)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[3]} onPress={this.pushToStack.bind(this, 3)}></Button></View>
         </View>
         <View style={styles.row}>
-          <View style={styles.button}><Button title="4"></Button></View>
-          <View style={styles.button}><Button title="5"></Button></View>
-          <View style={styles.button}><Button title="6"></Button></View>
-          <View style={styles.button}><Button title="-"></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[4]} onPress={this.pushToStack.bind(this, 4)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[5]} onPress={this.pushToStack.bind(this, 5)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[6]} onPress={this.pushToStack.bind(this, 6)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[7]} onPress={this.pushToStack.bind(this, 7)}></Button></View>
         </View>
         <View style={styles.row}>
-          <View style={styles.button}><Button title="1"></Button></View>
-          <View style={styles.button}><Button title="2"></Button></View>
-          <View style={styles.button}><Button title="3"></Button></View>
-          <View style={styles.button}><Button title="*"></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[8]} onPress={this.pushToStack.bind(this, 8)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[9]} onPress={this.pushToStack.bind(this, 9)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[10]} onPress={this.pushToStack.bind(this, 10)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[11]} onPress={this.pushToStack.bind(this, 11)}></Button></View>
         </View>
         <View style={styles.row}>
-          <View style={styles.threeFourthButton}><Button title="0"></Button></View>
-          <View style={styles.button}><Button title="/"></Button></View>
+          <View style={styles.threeFourthButton}><Button title={this.state.buttons[12]} onPress={this.pushToStack.bind(this, 12)}></Button></View>
+          <View style={styles.button}><Button title={this.state.buttons[13]} onPress={this.pushToStack.bind(this, 13)}></Button></View>
         </View>
         <View style={styles.button}>
           <View><Button title="="></Button></View>
