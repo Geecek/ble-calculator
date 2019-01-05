badff871689aff8178364e9765a6395376117046
Geecek <gicalamaciej@gmail.com>
2019-01-05 12:37:53 +0100

    Move connecting logic to search screen

--- screens/CalculatorScreen.js
+++ screens/CalculatorScreen.js
@@ -7,14 +7,11 @@ import {
   Platform
 } from 'react-native'
 
-import { BleManager } from 'react-native-ble-plx';
-
 import TabBarIcon from '../components/TabBarIcon';
 
 export default class CalculatorScreen extends Component {
   constructor(props) {
     super(props)
-    this.manager = new BleManager()
     this.state = {
       stack: [],
       buttons: ['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '*', '0', '/']
@@ -27,48 +24,6 @@ export default class CalculatorScreen extends Component {
       <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-calculator' : 'md-calculator'} />
     ),
   };
-
-  componentWillMount() {
-    const subscription = this.manager.onStateChange((state) => {
-      if (state === 'PoweredOn') {
-        this.scanAndConnect()
-        subscription.remove()
-      }
-    }, true)
-  }
-
-  scanAndConnect() {
-    this.manager.startDeviceScan(null, null, (error, device) => {
-      if (error) {
-        console.warn(error)
-        return
-      }
-      // console.warn(`Device: ${device.id}, ${device.name}, isConnectable: ${device.isConnectable}`)
-      // console.warn(Object.keys(device))
-      if (device.name === 'Koncowka') {
-        console.warn('Znalazlem twoj modul')
-        device.connect()
-          .then((device) => {
-            return device.discoverAllServicesAndCharacteristics()
-          })
-          .then((device) => {
-            device.writeCharacteristicWithResponseForService('0000FFE0-0000-1000-8000-00805F9B34FB', '0000FFE1-0000-1000-8000-00805F9B34FB', 'Mg==')
-            .then((characteristic) => {
-              console.warn('Wyslalem charakterystyke')
-              console.warn(Object.keys(characteristic))
-              console.warn(Object.entries(characteristic))
-            })
-            .catch((error) => {
-              console.warn('error: ', error)
-            })
-            console.warn(Object.keys(device))
-          })
-          .catch((error) => {
-            console.warn(error)
-          })
-      }
-    });
-  }
 
   pushToStack(index) {
     this.setState(({stack, buttons}) => {
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,15 +1,64 @@
 import React from 'react'
 import { View, Text, Platform } from 'react-native'
+import { BleManager } from 'react-native-ble-plx';
 
 import TabBarIcon from '../components/TabBarIcon'
 
 export default class SearchScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.manager = new BleManager()
+  }
+
   static navigationOptions = {
     title: 'Search',
     tabBarIcon: ({ focused }) => (
       <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'} />
     ),
   };
+
+  componentWillMount() {
+    console.warn(this.props.xd)
+    const subscription = this.manager.onStateChange((state) => {
+      if (state === 'PoweredOn') {
+        this.scanAndConnect()
+        subscription.remove()
+      }
+    }, true)
+  }
+
+  scanAndConnect() {
+    this.manager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.warn(error)
+        return
+      }
+      // console.warn(`Device: ${device.id}, ${device.name}, isConnectable: ${device.isConnectable}`)
+      // console.warn(Object.keys(device))
+      if (device.name === 'Koncowka') {
+        console.warn('Znalazlem twoj modul')
+        device.connect()
+          .then((device) => {
+            return device.discoverAllServicesAndCharacteristics()
+          })
+          .then((device) => {
+            device.writeCharacteristicWithResponseForService('0000FFE0-0000-1000-8000-00805F9B34FB', '0000FFE1-0000-1000-8000-00805F9B34FB', 'Mg==')
+            .then((characteristic) => {
+              console.warn('Wyslalem charakterystyke')
+              console.warn(Object.keys(characteristic))
+              console.warn(Object.entries(characteristic))
+            })
+            .catch((error) => {
+              console.warn('error: ', error)
+            })
+            console.warn(Object.keys(device))
+          })
+          .catch((error) => {
+            console.warn(error)
+          })
+      }
+    });
+  }
 
   render() {
     return (
